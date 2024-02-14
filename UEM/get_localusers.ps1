4eaff41ff58fedb3228825a2b62131dda7825c12
Adi-Nugraha-T <67138921+Adi-Nugraha-T@users.noreply.github.com>
2024-02-15 02:00:59 +0700

    Delete UEM/get_localusers.ps1

--- UEM/get_localusers.ps1
+++
@@ -1,8 +0,0 @@
-# Description: Return the current list of localusers.
-# Execution Context: SYSTEM
-# Execution Architecture: EITHER64OR32BIT
-# Return Type: STRING
-
-$listuser = Get-LocalUser
-$names = ($listuser.Name -join ";")
-write-output $names
